c491e179f3daee83b29526d04232fbe436a45bcc
Cengiz Krämer <cengiz.kraemer@meetrics.com>
2017-02-16 11:35:26 +0100

    browsersQuery

--- graphQLSchema/queries/browsersQuery.js
+++ graphQLSchema/queries/browsersQuery.js
@@ -1,15 +1,20 @@
 import {GraphQLList} from "graphql";
 import ImpressionsList from '../data/impressions';
 import Browser from '../objects/Browser';
+import _ from 'lodash';
+
+const BROWSER_REGEX = /(Firefox|Safari|Chrome|Chromium|Opera|MSIE )\/?/;
 
 export default {
   type: new GraphQLList(Browser),
   description: "List of browsers",
-  resolve: function () {
-    return ImpressionsList.map((impression) => {
-      return {
-        name: impression.userAgent
-      }
-    });
+  resolve: () => {
+    return _.uniq(ImpressionsList.map((impression) => {
+      var match = impression.userAgent.match(BROWSER_REGEX);
+      return match && match[1];
+    }).filter(a => !!a)).map((name) => {return {name}});
   }
 }
+
+
+
